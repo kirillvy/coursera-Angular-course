4b7784b01d024b2a5c9ffd797902086dd7170236
Kirill Vysokolov <kir2250@gmail.com>
2018-01-29 16:57:20 +0300

    router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule, MatListModule, MatGridListModule, MatLineModule, MatCardModule, MatButtonModule, MatIconModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component'
 
 import { DishService } from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+
+import { AppRoutingModule } from './app-routing/app-routing.module'
 
 @NgModule({
   declarations: [
@@ -21,7 +26,10 @@ import { FooterComponent } from './footer/footer.component'
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +40,8 @@ import { FooterComponent } from './footer/footer.component'
     MatGridListModule,
     MatLineModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [ DishService ],
   bootstrap: [AppComponent]
