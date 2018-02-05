968baa1753c2a6bed9d7629ec99446f06086952e
Kirill Vysokolov <kir2250@gmail.com>
2018-02-05 15:39:27 +0300

    rxjs part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -17,12 +17,16 @@ export class DishService {
     return Observable.of(DISHES).delay(2000);
   }
   getDish(id: number): Observable<Dish> {
-      return Observable.of(DISHES.filter((dish) => (dish.id == id))[0]).delay(2000);
+    return Observable.of(DISHES.filter((dish) => (dish.id == id))[0]).delay(2000);
   }
 
   getFeaturedDish(): Observable<Dish> {
-      return Observable.of(DISHES.filter((dish) => (dish.featured))[0]).delay(2000);
+    return Observable.of(DISHES.filter((dish) => (dish.featured))[0]).delay(2000);
 
+  }
+
+  getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id)).delay(2000);
   }
 
 }
